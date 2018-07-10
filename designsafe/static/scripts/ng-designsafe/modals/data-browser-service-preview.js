7e3f67a193e0b7fda51a0692e3ecef27ddf9c046
Frank Netscher <fnetscher@wireless-10-146-254-87.public.utexas.edu>
2018-07-10 16:49:43 -0500

    Converted the modal call and added data bindings to modal.

--- designsafe/static/scripts/ng-designsafe/modals/data-browser-service-preview.js
+++ designsafe/static/scripts/ng-designsafe/modals/data-browser-service-preview.js
@@ -1,99 +1,94 @@
 import template from '/static/scripts/ng-designsafe/html/modals/data-browser-service-preview.html';
 
+/**
+ * @param {FileListing} file
+ * @return {Promise}
+ */
 class Preview{
-  /**
-   *
-   * @param {FileListing} file
-   * @return {Promise}
-   */
-  constructor preview(file, listing) {
-    var modal = $uibModal.open({
-      controller: ['$scope', '$uibModalInstance', '$sce', 'file', function ($scope, $uibModalInstance, $sce, file) { 
-        //////////////////////////
-        constructor(file, listing){
-        $scope.file = file;
-        if (typeof listing !== 'undefined' &&
-          typeof listing.metadata !== 'undefined' &&
-          !_.isEmpty(listing.metadata.project)) {
-          let _listing = angular.copy(listing);
-          $scope.file.metadata = _listing.metadata;
-        }
-        $scope.busy = true;
-      }
-        file.preview().then(
-          (data)=>{
-            $scope.previewHref = $sce.trustAs('resourceUrl', data.href);
-            $scope.busy = false;
-          }, (err)=>{
-            let fileExt = file.name.split('.').pop();
-            let videoExt = ['webm', 'ogg', 'mp4'];
+  constructor (file, listing) {
+    $scope.file = file;
+    if (typeof listing !== 'undefined' &&
+        typeof listing.metadata !== 'undefined' &&
+        !_.isEmpty(listing.metadata.project)) {
+      let _listing = angular.copy(listing);
+      $scope.file.metadata = _listing.metadata;
+    }
+    $scope.busy = true;
+        
+    file.preview().then( 
+      (data)=>{
+        $scope.previewHref = $sce.trustAs('resourceUrl', data.href);
+        $scope.busy = false;
+      }, (err)=>{
+        let fileExt = file.name.split('.').pop();
+        let videoExt = ['webm', 'ogg', 'mp4'];
 
-            //check if preview is video
-            if (videoExt.includes(fileExt)) {
-              $scope.prevVideo = true;
-              file.download().then(
-                (data)=>{
-                  let postit = data.href;
-                  let oReq = new XMLHttpRequest();
-                  oReq.open("GET", postit, true);
-                  oReq.responseType = 'blob';
+        //check if preview is video
+        if (videoExt.includes(fileExt)) {
+          $scope.prevVideo = true;
+          file.download().then(
+            (data)=>{
+              let postit = data.href;
+              let oReq = new XMLHttpRequest();
+              oReq.open("GET", postit, true);
+              oReq.responseType = 'blob';
 
-                  oReq.onload = ()=>{
-                    if (this.status === 200) {
-                      let videoBlob = this.response;
-                      let vid = URL.createObjectURL(videoBlob);
+              oReq.onload = ()=>{
+                if (this.status === 200) {
+                  let videoBlob = this.response;
+                  let vid = URL.createObjectURL(videoBlob);
 
-                      // set video source and mimetype
-                      document.getElementById("videoPlayer").src = vid;
-                      document.getElementById("videoPlayer").setAttribute('type', `video/${fileExt}`);
-                    };
-                  };
-                  oReq.onerror = ()=>{
-                    $scope.previewError = err.data;
-                    $scope.busy = false;
-                  };
-                  oReq.send();
-                  $scope.busy = false;
-                },
-                (err)=>{
-                  $scope.previewError = err.data;
-                  $scope.busy = false;
-                });
-              // if filetype is not video or ipynb
-            } else if (fileExt != 'ipynb') {
+                  // set video source and mimetype
+                  document.getElementById("videoPlayer").src = vid;
+                  document.getElementById("videoPlayer").setAttribute('type', `video/${fileExt}`);
+                };
+              };
+              oReq.onerror = ()=>{
+                $scope.previewError = err.data;
+                $scope.busy = false;
+              };
+              oReq.send();
+              $scope.busy = false;
+            },
+            (err)=>{
               $scope.previewError = err.data;
               $scope.busy = false;
-              // if filetype is ipynb
-            } else {
-              file.download().then(
-                (data)=>{
-                  let postit = data.href;
-                  let oReq = new XMLHttpRequest();
+            });
+          // if filetype is not video or ipynb
+        } else if (fileExt != 'ipynb') {
+          $scope.previewError = err.data;
+          $scope.busy = false;
+          // if filetype is ipynb
+        } else {
+          file.download().then(
+            (data)=>{
+              let postit = data.href;
+              let oReq = new XMLHttpRequest();
 
-                  oReq.open("GET", postit, true);
+              oReq.open("GET", postit, true);
 
-                  oReq.onload = (oEvent)=>{
-                    let blob = new Blob([oReq.response], { type: "application/json" });
-                    let reader = new FileReader();
+              oReq.onload = (oEvent)=>{
+                let blob = new Blob([oReq.response], { type: "application/json" });
+                let reader = new FileReader();
 
-                    reader.onload = (e)=>{
-                      let content = JSON.parse(e.target.result);
-                      let target = $('.nbv-preview')[0];
-                      nbv.render(content, target);
-                    };
+                reader.onload = (e)=>{
+                  let content = JSON.parse(e.target.result);
+                  let target = $('.nbv-preview')[0];
+                  nbv.render(content, target);
+                };
 
-                    reader.readAsText(blob);
-                  };
+                reader.readAsText(blob);
+              };
 
-                  oReq.send();
-                },
-                (err)=>{
-                  $scope.previewError = err.data;
-                  $scope.busy = false;
-                });
-            }
-          }
-        );
+              oReq.send();
+            },
+            (err)=>{
+              $scope.previewError = err.data;
+              $scope.busy = false;
+            });
+        }
+      }
+    );}
 
         $scope.tests = allowedActions([file]);
 
@@ -127,12 +122,16 @@ class Preview{
           $uibModalInstance.dismiss();
         };
 
-      }],
-      size: 'lg',
-      resolve: {
-        file: ()=>{ return file; }
-      }
-    });
-    return modal.result;
-  }
-}
+      };
+
+const previewModal = {
+  template: template,
+  bindings: {
+    resolve: '<',
+    close: '&',
+    dismiss: '&'
+  },
+  controller: Preview,
+};
+
+export default previewModal;
